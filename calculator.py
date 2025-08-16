aecb4c94447a0d2cbfd34bc7b2a1e6834025d54c
Prerak siddhpura <prerak.siddhpura@gmail.com>
2025-08-16 16:58:42 +0530

    initial commit

---
+++ calculator.py
@@ -0,0 +1,20 @@
+import math
+
+num1 = float(input("Enter number one: "));
+num2 = float(input("Enter number two: "));
+
+print(("{}+{}").format(num1,num2));
+print(num1 + num2);
+
+print(("{}-{}").format(num1,num2));
+print(num1 - num2);
+
+print(("{}*{}").format(num1,num2));
+print(num1 * num2);
+
+print(("{}/{}").format(num1,num2));
+print(num1 / num2);
+
+print(("{}&{}").format(num1,num2));
+print(num1 & num2);
+
